d2827696b61e427c865a4479596d3ff21f79ce53
mac <mac@esco-3.local>
2022-02-21 19:50:12 +0000

    init

---
+++ location.js
@@ -0,0 +1,23 @@
+const locations = {
+  "5.603505243975896, -0.18591264372885277": {
+    restaurants: [
+      {
+        name: "regal",
+        location: {
+          lat: 1,
+          lng: 2,
+        },
+      },
+    ],
+  },
+};
+
+const fromLocation = {
+  latitude: 1.5496614931250685,
+  longitude: 110.36381866919922,
+};
+
+const toLocation = {
+  latitude: 1.5347282806345879,
+  longitude: 110.35632207358996,
+};
---
+++ locations.js
@@ -0,0 +1,8 @@
+const locations = {
+    legon: {
+        lat: 5.650839590132497,
+        long: -0.196149301551981 
+    }
+}
+
+export default locations;
